5993094a2ff0ade648eacc4c3a0e9408f18c3b77
João Prates <jvdprates@hotmail.com>
2020-09-06 15:02:49 -0300

    Progresso na parte 2 AP4

--- AtividadePratica4/parte2/Matrix.cpp
+++ AtividadePratica4/parte2/Matrix.cpp
@@ -1,25 +1,119 @@
 #include "matrix.h"
+#include <iostream>
 
-Matrix::Matrix(){};
+using std::cin;
+using std::cout;
+using std::endl;
 
-Matrix::Matrix(int rows, int cols, const double &value = 0.0){};
+Matrix::Matrix() : nRows(0), nCols(0)
+{
+    cout << "Construtor padrao" << endl;
+    this->m = new double *[1];
+    this->m[0] = new double[1];
+};
 
-Matrix::Matrix(ifstream &myFile){};
+Matrix::Matrix(int rows, int cols, const double &value) : nRows(rows), nCols(cols)
+{
+    cout << "Construtor por parametros" << endl;
+    this->m = new double *[cols];
+    for (int i = 0; i < cols; i++)
+    {
+        this->m[i] = new double[rows];
+        for (int j = 0; j < rows; j++)
+        {
+            this->m[i][j] = value;
+        }
+    }
+};
 
-Matrix::Matrix(const Matrix &that){};
+Matrix::Matrix(ifstream &myFile)
+{
+    cout << "Construtor por arquivo" << endl;
+};
 
-Matrix::~Matrix(){};
+Matrix::Matrix(const Matrix &that) : nRows(that.nRows), nCols(that.nCols)
+{
+    cout << "Construtor por copia" << endl;
+    this->m = new double *[this->nCols];
+    for (int i = 0; i < this->nCols; i++)
+    {
+        this->m[i] = new double[this->nRows];
+        for (int j = 0; j < this->nRows; j++)
+        {
+            this->m[i][j] = that.m[i][j];
+        }
+    }
+};
 
-int Matrix::getRows() const {};
+Matrix::~Matrix()
+{
+    cout << "Destrutor" << endl;
+    for (int i = 0; i < this->nCols; i++)
+    {
+        delete this->m[i];
+    }
 
-int Matrix::getCols() const {};
+    delete[] this->m;
+};
 
-double Matrix::get(int row, int col) const {};
+int Matrix::getRows() const { return this->nRows; };
 
-void Matrix::print() const {};
+int Matrix::getCols() const { return this->nCols; };
+
+double Matrix::get(int row, int col) const
+{
+    if (row <= this->nRows && col <= this->nCols)
+    {
+        return this->m[row][col];
+    }
+    else
+    {
+        return 0;
+    }
+};
+
+void Matrix::print() const
+{
+    for (int i = 0; i < this->nCols; i++)
+    {
+        cout << "[";
+        for (int j = 0; j < this->nRows; j++)
+        {
+            if (j != this->nRows - 1)
+            {
+                cout << this->m[i][j] << " ,";
+            }
+            else
+            {
+                cout << this->m[i][j];
+            }
+        }
+        cout << "]" << endl;
+    }
+};
 
 void Matrix::unit(){};
 
-void Matrix::zeros(){};
+void Matrix::zeros()
+{
+    for (int i = 0; i < this->nCols; i++)
+    {
+        this->m[i] = new double[this->nRows];
+        for (int j = 0; j < this->nRows; j++)
+        {
+            this->m[i][j] = 0;
+        }
+    }
+};
 
-void Matrix::ones(){};
+void Matrix::ones()
+{
+    for (int i = 0; i < this->nCols; i++)
+    {
+        this->m[i] = new double[this->nRows];
+        for (int j = 0; j < this->nRows; j++)
+        {
+            this->m[i][j] = 1;
+        }
+    }
+};
--- AtividadePratica4/parte2/main.cpp
+++ AtividadePratica4/parte2/main.cpp
@@ -6,5 +6,12 @@ using std::cout;
 using std::endl;
 
 int main(){
+    Matrix a(2,3,4.5), b(1,2,2.67), c(10,10,3), d(3,3);
+    Matrix e = a;
+    a.print();
+    b.print();
+    c.print();
+    d.print();
+    e.print();
     return 0;
 }
